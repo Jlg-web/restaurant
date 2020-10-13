8a874ca1e054e5594be7c9549710ed764278b5e8
Julian Le Guen <julianleguen@MacBook-Pro-de-Julian-2.local>
2020-10-13 09:08:36 +0200

    markers

--- assets/app.js
+++ assets/app.js
@@ -1,93 +1,100 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
-const url = 'assets/list-restaurant.json';
+const nameRestaurant = document.createElement('h2');
+const address = document.createElement('p');
+let map;
 
-fetch(url)
-  .then((response) => response.json())
-  .then((response) => {
-    infoRestaurant(response);
-  });
+const fetchSearch = async () => {
+  return fetch('assets/list-restaurant.json')
+    .then(res => res.json())
+    .then(res => res.restaurants) 
+};
 
-// MAP
+  // Function Add Marker
+  function addMarker(props, map) {
+    let marker = new google.maps.Marker({
+      position: props.coords,
+      map: map
+    })
+  }
+
+//*** AFFICHAGE MAP ***/
 function initMap() {
+  
+  // Map options
+  let optionsMap = {
+    zoom: 11,
+    center: {
+      lat: 48.8833024,
+      lng: 2.5329664
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
 
-  // Localisation par défaut
-  let defaultLocation = {
-    lat: 48.8737815,
-    lng: 2.3501649
-  };
+  // New map
+  map = new google.maps.Map(document.getElementById("map"), optionsMap);
 
-  // Affichage de la carte
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: defaultLocation,
-    zoom: 10,
-    mapTypeId: "roadmap"
+  // Listen click map
+  google.maps.event.addListener(map, 'click', function(event){
+    // Add marker
+    addMarker({coords:event.latLng});
   });
 
-  // Geolocalisation utilisateur
+  // Add user geoloc
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
+
       function (position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
 
-        map.setCenter(pos);
-        let marker = new google.maps.Marker({
-          position: pos,
+        const marker = new google.maps.Marker({
+          position: { lat: position.coords.latitude, lng: position.coords.longitude},
           map: map,
-          // icon: {
-          //   url: "http://maps.google.com/mapfiles/ms/icons/grn-pushpin.png"
-          // }
+          icon: 'assets/img/user.svg'
         });
-
       },
-      function () {
+      () => {
         handleLocationError(true, infoWindow, map.getCenter());
       }
     );
   } else {
-    // Browser doesn't support Geolocation
+    // Le navigateur ne prend pas en charge la géolocalisation
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-  // Ajout d'un marker au click
-  map.addListener("click", (e) => {
-    placeMarkerAndPanTo(e.latLng, map);
+  //Markers
+  // addMarker({ coords:{lat: 48.8737815,lng: 2.3501649 }});
+  // addMarker({ coords:{lat: 48.8551297,lng: 2.3610925 }});
+  // addMarker({ coords:{lat: 48.8468771,lng: 2.34899663 }});
+  // addMarker({ coords:{lat: 48.8865035,lng: 2.3442197 }});
+  // addMarker({ coords:{lat: 48.8686307,lng: 2.3170153}});
+  // addMarker({ coords:{lat: 48.8860196,lng: 2.3487899}});
+
+}
+
+//*** AFFICHAGE RESTAURANTS ***/
+const display = async () => {
+
+  const restaurants = await fetchSearch();
+  console.log(restaurants);
+
+  restaurants.forEach(restaurant => {
+    addMarker({ coords:{lat: restaurant.lat,lng: restaurant.lon }}, map);
   });
 
-}
+  // Liste des restaurants
+  section.innerHTML = (
+    restaurants.map(restaurant => (
+      `
+      <article>
+      <div class="content-text">
+      <h2>${restaurant.restaurantName}</h2>
+       <p class="article-adress">Adresse : ${restaurant.address}</p>
+       </div>
+       </article>
+      `
+    )).join('')
+  );
+};
 
-// Ajout Marker et gestion
-function placeMarkerAndPanTo(latLng, map) {
-  new google.maps.Marker({
-    position: latLng,
-    map: map,
-  });
-  map.panTo(latLng);
-}
-
-
-function infoRestaurant(jsonObj) {
-
-  const resto = jsonObj['restaurants'];
-
-  for (let i = 0; i < resto.length; i++) {
-    const myArticle = document.createElement('article');
-
-    //Name
-    const nameRestaurant = document.createElement('h2');
-    nameRestaurant.textContent = resto[i].restaurantName;
-    myArticle.appendChild(nameRestaurant);
-
-    //Address
-    const address = document.createElement('p');
-    address.textContent = resto[i].address;
-    myArticle.appendChild(address);
-
-    section.appendChild(myArticle);
-  }
-}
-
-
+// Appel fonction
+display();
